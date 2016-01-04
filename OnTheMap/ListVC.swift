e00aa1c90d1fb3a0315a4fb0036854f1c671d6f5
Robert Garza <robstudent1988@gmail.com>
2016-01-03 23:24:34 -0800

    Added ListVC: UITableViewController, created logout functionality for both ListVC and MapVC.

---
+++ OnTheMap/ListVC.swift
@@ -0,0 +1,47 @@
+//
+//  ListVC.swift
+//  OnTheMap
+//
+//  Created by Robert Garza on 1/3/16.
+//  Copyright © 2016 Robert Garza. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logout(sender: UIBarButtonItem) {
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        request.HTTPMethod = "DELETE"
+        var xsrfCookie: NSHTTPCookie? = nil
+        let sharedCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        for cookie in sharedCookieStorage.cookies as [NSHTTPCookie]! {
+            if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+        }
+        if let xsrfCookie = xsrfCookie {
+            request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+        }
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            let newData = data!.subdataWithRange(NSMakeRange(5, data!.length - 5)) /* subset response data! */
+            print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+        }
+        task.resume()
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
