8b3e95eaa576123a08cc08111b4bb0ce5ff4661d
Robert Garza <robstudent1988@gmail.com>
2016-01-03 22:36:23 -0800

    changed UITextView's to UITextFields and refractored code to reflect that

--- OnTheMap/LoginVC.swift
+++ OnTheMap/LoginVC.swift
@@ -8,17 +8,16 @@
 
 import UIKit
 
-class LoginVC : UIViewController, UITextViewDelegate {
+class LoginVC : UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var emailTextView: UITextView!
-    @IBOutlet weak var passwordTextView: UITextView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        emailTextView.delegate = self
-        passwordTextView.delegate = self
-        passwordTextView.secureTextEntry = true
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,32 +25,35 @@ class LoginVC : UIViewController, UITextViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    //MARK: - UITextViewDelegate
-    
-    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
-        if(text == "\n") {
-            textView.resignFirstResponder()
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        if(string == "\n"){
+            textField.resignFirstResponder()
             return false
         }
         return true
     }
     
-    func textViewDidBeginEditing(textView: UITextView) {
-        if(textView.text == "Email" || textView.text == "Password"){
-            textView.text = ""
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if(textField.text == "Email" || textField.text == "Password"){
+            textField.text = ""
         }
     }
-
+    
     //MARK: - IBActions
     
     @IBAction func loginVerify(sender: UIButton) {
-        
+        guard let emailText = emailTextField.text else {
+            return
+        }
+        guard let passwordText = passwordTextField.text else {
+            return
+        }
         //Call the Udacity API
         let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
         request.HTTPMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.HTTPBody = "{\"udacity\": {\"username\": \"\(emailTextView.text)\", \"password\": \"\(passwordTextView.text)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+        request.HTTPBody = "{\"udacity\": {\"username\": \"\(emailText)\", \"password\": \"\(passwordText)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
         
         //Session
         let session = NSURLSession.sharedSession()
@@ -76,9 +78,15 @@ class LoginVC : UIViewController, UITextViewDelegate {
             
             //Perform the error check on the main thread or there will be an error in attempting to present a new UIViewController/AlertViewController
             NSOperationQueue.mainQueue().addOperationWithBlock {
-                if let errorText = String(data: newData, encoding: NSUTF8StringEncoding) where errorText.rangeOfString("403") != nil {
+                if let errorText = String(data: newData, encoding: NSUTF8StringEncoding) where errorText.rangeOfString("error") != nil {
+                    var errorString:String?
                     //Show invalid info alerty when 403 error occures
-                    let alert = UIAlertController(title: "Invalid Information", message: "There was something wrong with your input Email/Password", preferredStyle: .Alert)
+                    if errorText.rangeOfString("403") != nil {
+                        errorString = "There was something wrong with your input Email/Password"
+                    } else {
+                        errorString = errorText
+                    }
+                    let alert = UIAlertController(title: "Invalid Information", message: errorString, preferredStyle: .Alert)
                     let OkAction = UIAlertAction(title: "OK", style: .Default){(action) in
                         self.dismissViewControllerAnimated(true, completion: nil)
                     }
